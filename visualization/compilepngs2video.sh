8b60cb4d3c281ad13cee8b2c37b70ac39d0d5b6c
Ross Mauck <ross@rossmauck.com>
2018-03-01 13:03:38 -0800

    Update compilepngs2video.sh
    changed file output name from test.mp4 to output-prob2D.mp4

--- visualization/compilepngs2video.sh
+++ visualization/compilepngs2video.sh
@@ -1,1 +1,1 @@
-ffmpeg -r 30 -f image2 -s 800x800 -i figures/prob2D_%01d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p test.mp4
+ffmpeg -r 30 -f image2 -s 800x800 -i figures/prob2D_%01d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p output-prob2D.mp4
